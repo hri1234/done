a8ef28ef59aa6e6d20d3f2807f843d01f6ee5b85
hritik pandey <128885231+hri1234@users.noreply.github.com>
2023-10-18 15:50:27 +0530

    Update app.js
    2nd commit

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ let allowedOrigins = [
 
 
 // limit the number of requests from a single IP address
-app.use('proxy',true)
+//app.use('proxy',true)
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // Limit each IP to 20 requests per `window` (here, per 15 minutes)
